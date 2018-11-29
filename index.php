b0ea851558d3c03ac339a4538b1a09e106f175f0
Laureta Dzika <xhika_l@hotmail.com>
2018-11-29 23:18:24 +0100

    added basic layout

---
+++ index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<link href="https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css" rel="stylesheet">
+	<title>derp</title>
+</head>
+<body>
+	<div class="w-full bg-indigo h-12 mx-auto text-center">
+		<h1 class="text-white">Derp</h1>
+	</div>
+
+	<div class="max-w-lg flex flex-wrap">
+		<div class="card w-1/4 h-48 rounded-lg border-2 border-indigo">
+			<img class="front-card rounded" src="assets/img/surprised_open_mouth.jpg">
+			<img class="back-card rounded" src="assets/img/freddie_mercury.jpg">
+		</div>
+		<div class="card w-1/4 h-48 rounded-lg border-2 border-indigo">
+			<img class="front-card rounded" src="assets/img/me_gusta.jpg">
+			<img class="back-card rounded" src="assets/img/freddie_mercury.jpg">
+		</div>
+		<div class="card w-1/4 h-48 rounded-lg border-2 border-indigo">
+			<img class="front-card rounded" src="assets/img/no.jpg">
+			<img class="back-card rounded" src="assets/img/freddie_mercury.jpg">
+		</div>
+		<div class="card w-1/4 h-48 rounded-lg border-2 border-indigo">
+			<img class="front-card rounded" src="assets/img/oh_stop.jpg">
+			<img class="back-card rounded" src="assets/img/freddie_mercury.jpg">
+		</div>
+		<div class="card w-1/4 h-48 rounded-lg border-2 border-indigo">
+			<img class="front-card rounded" src="assets/img/pokerface.jpg">
+			<img class="back-card rounded" src="assets/img/freddie_mercury.jpg">
+		</div>
+		<div class="card w-1/4 h-48 rounded-lg border-2 border-indigo">
+			<img class="front-card rounded" src="assets/img/spiderpman.jpg">
+			<img class="back-card rounded" src="assets/img/freddie_mercury.jpg">
+		</div>
+		<div class="card w-1/4 h-48 rounded-lg border-2 border-indigo">
+			<img class="front-card rounded" src="assets/img/surprised_open_mouth.jpg">
+			<img class="back-card rounded" src="assets/img/freddie_mercury.jpg">
+		</div>
+		<div class="card w-1/4 h-48 rounded-lg border-2 border-indigo">
+			<img class="front-card rounded" src="assets/img/troll_face.jpg">
+			<img class="back-card rounded" src="assets/img/freddie_mercury.jpg">
+		</div>
+		<div class="card w-1/4 h-48 rounded-lg border-2 border-indigo">
+			<img class="front-card rounded" src="assets/img/y_u_no.jpg">
+			<img class="back-card rounded" src="assets/img/freddie_mercury.jpg">
+		</div>
+		<div class="card w-1/4 h-48 rounded-lg border-2 border-indigo">
+			<img class="front-card rounded" src="assets/img/me_gusta.jpg">
+			<img class="back-card rounded" src="assets/img/freddie_mercury.jpg">
+		</div>
+		<div class="card w-1/4 h-48 rounded-lg border-2 border-indigo">
+			<img class="front-card rounded" src="assets/img/no.jpg">
+			<img class="back-card rounded" src="assets/img/freddie_mercury.jpg">
+		</div>
+		<div class="card w-1/4 h-48 rounded-lg border-2 border-indigo">
+			<img class="front-card rounded" src="assets/img/oh_stop.jpg">
+			<img class="back-card rounded" src="assets/img/freddie_mercury.jpg">
+		</div>
+		<div class="card w-1/4 h-48 rounded-lg border-2 border-indigo">
+			<img class="front-card rounded" src="assets/img/pokerface.jpg">
+			<img class="back-card rounded" src="assets/img/freddie_mercury.jpg">
+		</div>
+		<div class="card w-1/4 h-48 rounded-lg border-2 border-indigo">
+			<img class="front-card rounded" src="assets/img/spiderpman.jpg">
+			<img class="back-card rounded" src="assets/img/freddie_mercury.jpg">
+		</div>
+		<div class="card w-1/4 h-48 rounded-lg border-2 border-indigo">
+			<img class="front-card rounded" src="assets/img/troll_face.jpg">
+			<img class="back-card rounded" src="assets/img/freddie_mercury.jpg">
+		</div>
+		<div class="card w-1/4 h-48 rounded-lg border-2 border-indigo">
+			<img class="front-card rounded" src="assets/img/y_u_no.jpg">
+			<img class="back-card rounded" src="assets/img/freddie_mercury.jpg">
+		</div>
+	</div>
+	<div class="mx-auto text-center">
+		<div class="lds-ripple"><div></div><div></div></div>
+		<button class="bg-indigo py-4 px-6 text-white rounded uppercase font-bold tracking-wide shadow-lg my-8">Shuffle 
+		</button>
+	</div>
+
+
+	<script src="assets/script/main.js"></script>
+</body>
+</html>
