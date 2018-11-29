ea53d3be9b7264e4744eaa305b2e0ea25b74e3f0
Laureta Dzika <xhika_l@hotmail.com>
2018-11-29 23:31:25 +0100

    added styling and javascript
    added some styling for cards
    added javascript event to call function and toggle class 'flip' for every card clicked

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,1 +1,10 @@
 'use strict'
+
+const cards = document.querySelectorAll('.card')
+
+// toggles flip class to every card
+function flipCard() {
+	this.classList.toggle('flip');
+}
+// adds click event for every card using callback flipCard
+cards.forEach(card => card.addEventListener('click', flipCard));
