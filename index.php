f5dc9d69da97a951a2270cf2d5023ba0b2d5ba3c
Laureta Dzika <xhika_l@hotmail.com>
2018-11-30 13:36:10 +0100

    minor styling changes

--- index.php
+++ index.php
@@ -5,81 +5,82 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
 	<link href="https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Leckerli+One" rel="stylesheet">
 	<title>derp</title>
 </head>
 <body class="bg-grey-light">
 	<div class="w-full bg-indigo h-12 mx-auto text-center">
-		<h1 class="text-white pt-1">Memory Game</h1>
+		<h1 class="text-white pt-1 font-hairline">Le memory game</h1>
 	</div>
 
 	<div class="max-w-lg flex flex-wrap my-8">
-		<div class="card w-1/4 h-48 rounded-lg border-2 border-indigo" data-img="surprised_open_mouth">
+		<div class="card m-2 rounded-lg border-2 border-indigo" data-img="surprised_open_mouth">
 			<img class="front-card rounded" src="assets/img/surprised_open_mouth.jpg">
 			<img class="back-card rounded" src="assets/img/freddie_mercury.jpg">
 		</div>
-		<div class="card w-1/4 h-48 rounded-lg border-2 border-indigo" data-img="me_gusta">
+		<div class="card m-2 rounded-lg border-2 border-indigo" data-img="me_gusta">
 			<img class="front-card rounded" src="assets/img/me_gusta.jpg">
 			<img class="back-card rounded" src="assets/img/freddie_mercury.jpg">
 		</div>
-		<div class="card w-1/4 h-48 rounded-lg border-2 border-indigo" data-img="no">
+		<div class="card m-2 rounded-lg border-2 border-indigo" data-img="no">
 			<img class="front-card rounded" src="assets/img/no.jpg">
 			<img class="back-card rounded" src="assets/img/freddie_mercury.jpg">
 		</div>
-		<div class="card w-1/4 h-48 rounded-lg border-2 border-indigo" data-img="oh_stop">
+		<div class="card m-2 rounded-lg border-2 border-indigo" data-img="oh_stop">
 			<img class="front-card rounded" src="assets/img/oh_stop.jpg">
 			<img class="back-card rounded" src="assets/img/freddie_mercury.jpg">
 		</div>
-		<div class="card w-1/4 h-48 rounded-lg border-2 border-indigo" data-img="pokerface">
+		<div class="card m-2 rounded-lg border-2 border-indigo" data-img="pokerface">
 			<img class="front-card rounded" src="assets/img/pokerface.jpg">
 			<img class="back-card rounded" src="assets/img/freddie_mercury.jpg">
 		</div>
-		<div class="card w-1/4 h-48 rounded-lg border-2 border-indigo" data-img="spiderpman">
+		<div class="card m-2 rounded-lg border-2 border-indigo" data-img="spiderpman">
 			<img class="front-card rounded" src="assets/img/spiderpman.jpg">
 			<img class="back-card rounded" src="assets/img/freddie_mercury.jpg">
 		</div>
-		<div class="card w-1/4 h-48 rounded-lg border-2 border-indigo" data-img="surprised_open_mouth">
+		<div class="card m-2 rounded-lg border-2 border-indigo" data-img="surprised_open_mouth">
 			<img class="front-card rounded" src="assets/img/surprised_open_mouth.jpg">
 			<img class="back-card rounded" src="assets/img/freddie_mercury.jpg">
 		</div>
-		<div class="card w-1/4 h-48 rounded-lg border-2 border-indigo" data-img="troll_face">
+		<div class="card m-2 rounded-lg border-2 border-indigo" data-img="troll_face">
 			<img class="front-card rounded" src="assets/img/troll_face.jpg">
 			<img class="back-card rounded" src="assets/img/freddie_mercury.jpg">
 		</div>
-		<div class="card w-1/4 h-48 rounded-lg border-2 border-indigo" data-img="y_u_no">
+		<div class="card m-2 rounded-lg border-2 border-indigo" data-img="y_u_no">
 			<img class="front-card rounded" src="assets/img/y_u_no.jpg">
 			<img class="back-card rounded" src="assets/img/freddie_mercury.jpg">
 		</div>
-		<div class="card w-1/4 h-48 rounded-lg border-2 border-indigo" data-img="me_gusta">
+		<div class="card m-2 rounded-lg border-2 border-indigo" data-img="me_gusta">
 			<img class="front-card rounded" src="assets/img/me_gusta.jpg">
 			<img class="back-card rounded" src="assets/img/freddie_mercury.jpg">
 		</div>
-		<div class="card w-1/4 h-48 rounded-lg border-2 border-indigo" data-img="no">
+		<div class="card m-2 rounded-lg border-2 border-indigo" data-img="no">
 			<img class="front-card rounded" src="assets/img/no.jpg">
 			<img class="back-card rounded" src="assets/img/freddie_mercury.jpg">
 		</div>
-		<div class="card w-1/4 h-48 rounded-lg border-2 border-indigo" data-img="oh_stop">
+		<div class="card m-2 rounded-lg border-2 border-indigo" data-img="oh_stop">
 			<img class="front-card rounded" src="assets/img/oh_stop.jpg">
 			<img class="back-card rounded" src="assets/img/freddie_mercury.jpg">
 		</div>
-		<div class="card w-1/4 h-48 rounded-lg border-2 border-indigo" data-img="pokerface">
+		<div class="card m-2 rounded-lg border-2 border-indigo" data-img="pokerface">
 			<img class="front-card rounded" src="assets/img/pokerface.jpg">
 			<img class="back-card rounded" src="assets/img/freddie_mercury.jpg">
 		</div>
-		<div class="card w-1/4 h-48 rounded-lg border-2 border-indigo" data-img="spiderpman">
+		<div class="card m-2 rounded-lg border-2 border-indigo" data-img="spiderpman">
 			<img class="front-card rounded" src="assets/img/spiderpman.jpg">
 			<img class="back-card rounded" src="assets/img/freddie_mercury.jpg">
 		</div>
-		<div class="card w-1/4 h-48 rounded-lg border-2 border-indigo" data-img="troll_face">
+		<div class="card m-2 rounded-lg border-2 border-indigo" data-img="troll_face">
 			<img class="front-card rounded" src="assets/img/troll_face.jpg">
 			<img class="back-card rounded" src="assets/img/freddie_mercury.jpg">
 		</div>
-		<div class="card w-1/4 h-48 rounded-lg border-2 border-indigo" data-img="y_u_no">
+		<div class="card m-2 rounded-lg border-2 border-indigo" data-img="y_u_no">
 			<img class="front-card rounded" src="assets/img/y_u_no.jpg">
 			<img class="back-card rounded" src="assets/img/freddie_mercury.jpg">
 		</div>
 	</div>
 	<div class="mx-auto text-center">
-		<button class="shuffle bg-indigo py-4 px-6 text-white rounded uppercase font-bold tracking-wide shadow-lg my-8">Shuffle 
+		<button class="shuffle bg-indigo hover:bg-indigo-dark py-4 px-6 text-white rounded uppercase font-bold tracking-wide shadow-lg my-2">Shuffle 
 		</button>
 	</div>
 
